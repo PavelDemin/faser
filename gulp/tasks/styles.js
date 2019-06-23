b219046b22bc99904bfd37e76039f27d44622b1f
PavelDemin <papyasishe@yandex.ru>
2019-06-24 02:31:40 +0300

    add new section

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -24,6 +24,7 @@ module.exports = function () {
             .pipe($.gulp.dest(stylesPATH.ouput))
             .on('end', $.browserSync.reload);
     });
+
     $.gulp.task('styles:build', () => {
         return $.gulp.src(stylesPATH.input + 'styles.scss')
             .pipe(scss())
